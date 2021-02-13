9e79eef4d917ae48feac9ba1273230c2cd4e0034
yosep34 <yosepalexsander9@gmail.com>
2021-02-13 23:06:51 +0700

    change createConnection with createPooling

---
+++ 4/4b/helpers/db.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+
+const pool = mysql.createPool({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+});
+
+// connection.connect((err) => {
+//   if (!err) {
+//     console.log('Connected!');
+//   } else {
+//     console.log('Connection Failed!', err);
+//   }
+// });
+
+module.exports = pool;
